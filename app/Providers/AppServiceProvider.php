6e7d1ce76f98dbee8297bae91cbc80c800d03d02
tiheikko <tiheikko@gmail.com>
2024-10-17 17:42:20 +0300

    updated files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use App\Models\BirdsStatistic;
+use App\Observers\BirdsStatisticsObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+        BirdsStatistic::observe(BirdsStatisticsObserver::class);
     }
 }
